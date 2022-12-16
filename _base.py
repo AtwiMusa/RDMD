4f6f96437db34399535fab9f83e1a56ac9654c33
Musa Atwi <108957652+AtwiMusa@users.noreply.github.com>
2022-12-16 18:22:13 +0100

    Modified base.py

--- _base.py
+++ _base.py
@@ -81,45 +81,53 @@ class Gaussian(Observable): # haide betrajje3 bs col
         self.rho = rho
        
     def _evaluate(self, x: np.ndarray) -> np.ndarray:
-        ##psi = np.zeros((x.shape[0],1))
-        ##for i in range(0,x.shape[0]): 
-            ###for j in range(0,self.centers.shape[0]):
-                ###gb[i][j] = np.exp(-(norm(self.centers[j]-x[i])**2)/self.rho**2)
+        r"""
+        Returns the prediction of the NN as a column vector.
+        
+        Parameters
+        ----------
+        x : (N, d) np.ndarray
+            Evaluates the observable for N d-dimensional data points.
 
-            ##psi[i] = np.exp(-(np.linalg.norm(self.centers-x[i])**2)/self.rho**2)
-        #gb = lambda x : np.exp(-(np.linalg.norm(self.centers-x)/self.rho)**2)
-        ###gb/gb.sum(axis=1)[:,None]
+        Returns
+        -------
+        psi : (N, 1) np.ndarray
+            Result of the prediction for each data point.
+        """
         psi=np.array([np.exp(-(np.linalg.norm(self.centers-e)**2)/self.rho**2) for e in x]) 
-        #psi = psi/np.sum(psi)
         return psi
 
 
-class Rinn(Observable):  # haide betrajje3 bs col
-    #y = w.x + b # Sequential as we have single input and single output otherwise we have funtion API
-    #for the input layer we dont need to define act func or ker initi its just send inputs to the 1st layer
-    def __init__(self, units, activations, input_dim): # for the output always better to use sigmoid 
+class Rinn(Observable): 
+    def __init__(self, units, activations, input_dim): 
+        # for the output it would be better to use sigmoid 
         self.model = Sequential(
-        [Input(shape = input_dim)] + [Dense(unit, activation = activation, kernel_initializer = "uniform") for unit, activation in zip(units, activations)]) # kern initi for dense layer hon unform         
+        [Input(shape = input_dim)] + [Dense(unit, activation = activation, kernel_initializer = "uniform") for unit, activation in zip(units, activations)]) 
+        
                
-    def _evaluate(self, x: np.ndarray, y: np.ndarray, x_test, y_test) -> np.ndarray:
-        #loss = tf.keras.losses.mean_squared_error
-        #optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001)
-        self.model.compile(optimizer="sgd", loss="mse", metrics=["mae", "acc"])
-        # fit the keras model on the dataset
-        self.model.fit(x, y, epochs=150, batch_size=10, verbose=0)
-        # make class predictions with the model
-        #predictions = (self.model.predict(x) > 0.5).astype(int)
-        yhat = self.model.predict(x_test)
-        # evaluate predictions
-        acc = accuracy_score(y_test, yhat)
+    def _evaluate(self, x: np.ndarray) -> np.ndarray:
+        r"""
+        Returns the prediction of the NN as a column vector.
+        
+        Parameters
+        ----------
+        x : (N, d) np.ndarray
+            Evaluates the observable for N d-dimensional data points.
+
+        Returns
+        -------
+        psi : (N, 1) np.ndarray
+            Result of the prediction for each data point.
+        """
         psi= np.array([self.model.predict(x)])
         psi = np.reshape(psi, (-1,1))
         return psi
     
 
-class Genbasis(Observable): # haide betrajje el matrix kella 
+class Genbasis(Observable): 
+    # This would give the whole matrix 
     
-    def __init__(self, basis, M, *hyperParams): #fi yb3at params adma bade
+    def __init__(self, basis, M, *hyperParams): 
         self.basis = basis
         self.M = M
         if self.basis == "Gaussianbasis":
@@ -128,19 +136,22 @@ class Genbasis(Observable): # haide betrajje el matrix kella
         elif self.basis == "Rinnbasis":
             self.units, self.activations = hyperParams[0], hyperParams[1]
             
-    def _evaluate(self, x: np.ndarray) -> np.ndarray:        
+    def _evaluate(self, x: np.ndarray) -> np.ndarray: 
+        r"""
+        psis: (N, M), where M is the number of basis functions
+        """
         psis = np.zeros((x.shape[0], self.M))
         if self.basis == "Gaussianbasis":
         
             for j in range(self.M):
                 psi = Gaussian(self.centers[j], self.rho)._evaluate(x)
-                psis[:,j] = psi.reshape((-1,)) # kl rows w col j
+                psis[:,j] = psi.reshape((-1,)) # all rows and just jth col
 
 
         elif self.basis == "Rinnbasis":
             
             for j in range(self.M):
-                psi = Rinn(self.units, self.activations, x.shape[1])._evaluate(x) #input layer 3m te5od no2ta 3nda tnen dim
+                psi = Rinn(self.units, self.activations, x.shape[1])._evaluate(x) #input layer takes 2-dim points
                 psis[:,j] = psi.reshape((-1,)) 
                 
         return psis
